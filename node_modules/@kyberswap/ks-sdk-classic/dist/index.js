696a4a93883983a0fb17774828feb7fbdc8bf82b
sharad jaiswal <sufiyan.conaxwebsolutions@gmail.com>
2023-01-24 13:58:50 +0530

    removing node_modules

--- node_modules/@kyberswap/ks-sdk-classic/dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./ks-sdk-classic.cjs.production.min.js')
-} else {
-  module.exports = require('./ks-sdk-classic.cjs.development.js')
-}
